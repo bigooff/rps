f91f094ec95e470dcd0d8d3bc46236666fdb5f12
skrtsman <gamisou10@gmail.com>
2022-06-19 02:42:58 +0300

    Finalized RPS-UI Code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // REMOVE COMMENT TAGS FROM LOGS FOR MORE INFO
 // RE WROTE FROM SCRATCH BY KEEPING THE BASIC LOGIC FROM THE OLD CODE
-
-
+const logBox = document.querySelector("#logger");
+const p = document.createElement("p");
+const scoreP = document.querySelector(".score");
+const roundP = document.querySelector(".round");
+const pComputerPick = document.createElement("p");
+const pEndGame = document.createElement("p");
 // RANDOM CHOICE GENERATOR
 function computerPlay(){
     const items = ["rock","paper","scissors"];
@@ -10,13 +14,16 @@ function computerPlay(){
     //console.log(items);
 
         if(choice === 0){
-            console.log("Computer pick rock")
+            pComputerPick.textContent = "Computer Pick: Rock";
+            logBox.appendChild(pComputerPick);
             return items[0];  
         } else if(choice === 1){
-            console.log("Computer pick Paper")
+            pComputerPick.textContent = "Computer Pick: Paper";
+            logBox.appendChild(pComputerPick);
             return items[1];
         } else{
-            console.log("Computer pick scissors")
+            pComputerPick.textContent = "Computer Pick: Scissors";
+            logBox.appendChild(pComputerPick);        
             return items[2];    
         }
 }
@@ -41,25 +48,34 @@ function computerSelection(){
 
 };
 // GET EACH BUTTON AND PLAY THE GAME WHEN PRESSED
-const choices = document.querySelectorAll("button");
-        for(let choice of choices){
-                  choice.addEventListener("click", game )}
+
+//console.log(choices)
+      
 //GET THE CORRESPONDING "CHOICE" ACCORDING TO THE BUTTONS TEXT_CONTENT
-function getChoice(){
-    for(let choice of choices){
-        return choice.textContent}
-}
+
+
+
+
+
+
 // GETS PLAYERS CHOICE WITH THE HELP OF GETCHOICE FUNCTION
 
- function playerSelection(){
-    choice = getChoice();
-     if(choice === "rock"){
+rockBtn.addEventListener('click', () => game('rock'));
+paperBtn.addEventListener('click', () => game('paper'));
+scissorsBtn.addEventListener('click', () => game('scissors'));
+
+
+ function playerSelection(playerChoice){
+ trueChoice = playerChoice;
+
+  console.log()
+     if(trueChoice === "rock"){
         console.log("Player pick rock!")
        return "rock";
-    } else if (choice === "paper"){
+    } else if (trueChoice === "paper"){
         console.log("Player pick paper!")
         return "paper";
-    } else if(choice === "scissors") {
+    } else if(trueChoice=== "scissors") {
         console.log("Player pick scissors!")
          return "scissors";
     } else{
@@ -68,55 +84,75 @@ function getChoice(){
   };        
 
 
-
     // ROUND PLAYER
 let playRoundCallCount = 0;
 
-function playRound(){
-    let a = playerSelection();
+function playRound(choice){
+    let a = playerSelection(choice);
      let b = computerSelection();
     // console.log(a);
     // console.log(b);
     playRoundCallCount += 1;
     console.log(playRoundCallCount+" ROUND")
         if(a === b){
-            console.log("Zero score points added because it is a tie!")
+            p.textContent = "Zero score points added because it is a tie!";
+            logBox.appendChild(p);
             return 0;   // tie
         } else if((a === "rock") && (b != "paper")){
-            console.log("My friend 1 score point for you and your rock");
+            p.textContent = "My friend 1 score point for you and your rock";
+            logBox.appendChild(p);
             return 1;   //rock
         } else if((a === "paper") && (b != "scissors")){  
-            console.log("Thats right 1 score point for you for this paper");  
+            p.textContent = "Thats right 1 score point for you for this paper";
+            logBox.appendChild(p); 
             return 1;   //paper
         } else if((a === "scissors") && (b != "rock")){
-            console.log("You know who it is the man the player 1 point for scissors");
+            p.textContent = "You know who it is the man the player 1 point for scissors";
+            logBox.appendChild(p);
             return 1;   //scissors
         } else {
-            console.log("Oh no you lose 1 point very bad stuff");
+            p.textContent = "Oh no you lose 1 point very bad stuff";
+            logBox.appendChild(p);
             return -1;   //lose
         }
 
 };
-//CALCULATES ROUND && SCORE -- OUTPUTS WINNER OR LOSER AFTER 5 ROUNDS
 
+
+//CALCULATES ROUND && SCORE -- OUTPUTS WINNER OR LOSER AFTER 5 ROUNDS
+const replayBtn = document.createElement("button")
+replayBtn.addEventListener("click",()=>{window.location.reload();})
+scoreP.textContent = `Score: 0`
+roundP.textContent = `Round: 0`
 let realScore = 0;
-function game(){
+function game(choice){
     let round = playRoundCallCount;
-    let scoreToCalculate = playRound();
-    console.log(scoreToCalculate)
-    
+    let scoreToCalculate = playRound(choice);
+    // console.log(scoreToCalculate)
     realScore += scoreToCalculate;
-    
-    console.log(realScore +" SCORE")
+    scoreP.textContent = `Score: ${realScore}`
+    roundP.textContent = `Round: ${round + 1}`
+    // console.log(realScore +" SCORE")
+
+
+
      if(round === 4){
         if((realScore === 0)){
-            console.log("ITS A TIE AMAZING ");
+            pEndGame.textContent = "TIE";
+            logBox.appendChild(pEndGame);
         } else if((realScore > 0)){
-            console.log("YOU WIN FINALLY!!!!")
+            pEndGame.textContent = "WIN";
+            logBox.appendChild(pEndGame);    
         } else if((realScore < 0)){
-            console.log("YOU LOSE MISERABLY")
-        }
+            pEndGame.textContent = "LOSE";
+            logBox.appendChild(pEndGame);     
+        } 
      }
-        
-   
+
+     if(round === 4){
+        replayBtn.classList.add("replayBtn")
+        replayBtn.textContent = "Replay"
+        logBox.appendChild(replayBtn);
+    }
+     
 }
