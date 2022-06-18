1a3c5bee0516d88e30217734cd4b7497bff26755
skrtsman <gamisou10@gmail.com>
2022-06-18 16:09:17 +0300

    Code clean up by removal of useless functions

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // RANDOM CHOICE GENERATOR
 // REMOVE COMMENT TAGS FROM LOGS FOR MORE INFO
 
+
+
+//NOTE TO SELF : IMPLEMENT THE EXECUTION OF THE GAME FUNCTION ACCORDING TO WHAT THE-
+// -PLAYERSELECTION FUNCTION RETURNS AFTER THE CLICK OF A BUTTON??? SEARCH GOOGLE ??
+
 function computerPlay(){
     const items = ["rock","paper","scissors"];
     const choice = Math.floor((items.length * (Math.random(3))));
@@ -21,7 +26,7 @@ function computerPlay(){
 
 
 // GETS RANDOM CHOICE AND TRANSLATES IT TO NUMBER
-// CODE IS BASED ON NUMBERS I DONT KNOW IF IT COULD BE DONE WITH STRINGS 
+// CODE IS BASED ON NUMBERS I DONT KNOW IF IT COULD BE DONE WITH STRINGS
 
 function computerSelection(){
     const pick = computerPlay();
@@ -39,14 +44,18 @@ function computerSelection(){
 
 };
 
+
+
 // gets choice from buttons
-const choices = document.querySelectorAll("button");
-    console.log(choices)
-    for(let choice of choices){
-        choice.addEventListener("click", () => {
-                playerSelection(choice.textContent)
-        })
-    }
+
+
+    const choices = document.querySelectorAll("button");
+        console.log(choices)
+        for(let choice of choices){
+                  choice.addEventListener("click", () => {
+                    playRound(playerSelection(choice.textContent),computerSelection())})
+                }
+
     // TAKES USER INPUT AND OUTPUTS IN NUMBER
  function playerSelection(choice){
     
@@ -65,68 +74,39 @@ const choices = document.querySelectorAll("button");
   }; 
 
   // ROUND PLAYER
+let playRoundCallCount = 0;
 
-
-function playRound(){
-    let a = playerSelection();
-    let b = computerSelection();
+function playRound(a , b){
+    // a = playerSelection
+    // b = computerSelection
     console.log(a);
     console.log(b);
+    playRoundCallCount += 1;
 
         if(a === b){
+            console.log("Zero score points added because it is a tie!")
             return 0;   // tie
         } else if((a === 1) && (b != 2)){
+            console.log("My friend 1 score point for you and your rock");
             return 1;   //rock
-        } else if((a === 2) && (b != 3)){    
+        } else if((a === 2) && (b != 3)){  
+            console.log("Thats right 1 score point for you for this paper");  
             return 2;   //paper
         } else if((a === 3) && (b != 1)){
+            console.log("You know who it is the man the player 1 point for scissors");
             return 3;   //scissors
         } else {
+            console.log("Oh no you lose 1 point very bad stuff");
             return 4;   //lose
         }
+
+        
 };
 
 
-function playWithButtons() {
-    buttonsChoice = [
-        rock = document.querySelector(".rock"),
-        paper = document.querySelector(".paper"),
-        scissors = document.querySelector(".scissors")
-    ];
-    for(let button of buttonsChoice){
-        button.addEventListener("click", playRound);
-    }
-    
-  
-}
 
 
 
-
-// IT MIGHT HAVE BEEN BETTTER TO JUST USE THE playRound() FUNCTION FOR THIS AND LOG FROM IN THERE
- 
-
-function score(){
-    const playerScore = playRound();
-   // console.log(playerScore);
-     if(playerScore === 0){
-         console.log("Zero score points added because it is a tie!")
-         return 0;
-     } else if (playerScore === 1){
-         console.log("My friend 1 score point for you and your rock");
-         return 1;
-     } else if (playerScore === 2){
-         console.log("Thats right 1 score point for you for this paper");
-         return 2;
-     } else if (playerScore === 3){
-         console.log("You know who it is the man the player 1 point for scissors");
-         return 3;
-     } else if (playerScore === 4){
-         console.log("Oh no you lose 1 point very bad stuff");
-         return 4;
-     }
-     
-}
 
 
 // sends out the values: +1 if it is a win, -1 if it is lose, 0 if it is a tie
